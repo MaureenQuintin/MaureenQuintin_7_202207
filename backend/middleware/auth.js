6d1d2bdd83c88dd71061919a1cb6748a6c50c507
Maureen Quintin <quintin.maureen@gmail.com>
2022-08-12 11:45:47 +0200

    correction bug back

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,22 +1,28 @@
 const jwt = require('jsonwebtoken');
 // Vérification que l'user est bien connecté
 module.exports = (req, res, next) => {
-   try {
-        const token = req.cookies.jwt;  // Récupération du token de l'user
+    let token = null;
+    if (req.cookies && req.cookies.jwt) {
+        token = req.cookies.jwt;
+    }  // Récupération du token de l'user
+    if (token) {
         const decodedToken = jwt.verify(token, `${process.env.TOKEN}`); // Vérification de la validité du token
         const userId = decodedToken.userId; // Récupération de l'userID dans le token
         req.auth = {
             userId: userId
         };
         next();
-   } catch(error) {
-    console.log('err', error);
-       res.status(401).json({ error });
-   }
+    } else {
+        res.status(401).json({error: 'Utilisateur non connecté' });
+
+    }
 };
 
 module.exports.requireAuth = (req, res, next) => {
-    const token = req.cookies.jwt;
+    let token = null;
+    if (req.cookies && req.cookies.jwt) {
+        token = req.cookies.jwt;
+    }
     if (token) {
         jwt.verify(token, `${process.env.TOKEN}`, async (err, decodedToken) => {
             if (err) {
@@ -31,6 +37,6 @@ module.exports.requireAuth = (req, res, next) => {
             }
       });
     } else {
-      console.log('No token');
+        res.status(401).json({error: 'Utilisateur non connecté' });
     }
   };
